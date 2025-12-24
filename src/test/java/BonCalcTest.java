8054bef220bd3a47450cffb1e71a74d0078c629d
Sergey Saryan <som-ana@yandex.ru>
2025-12-24 13:21:34 +0300

    initial  Fix #1

--- src/test/java/BonCalcTest.java
+++ src/test/java/BonCalcTest.java
@@ -5,9 +5,9 @@ public class BonCalcTest {
    @Test
    public void greatBonus(){
        Raschet raschet = new Raschet();
-           int expected = 50;
+           int expecteded = 50;
            int actual = raschet.bonus1(5000, false);
-           Assertions.assertEquals(expected,actual);
+           Assertions.assertEquals(expecteded,actual);
        }
 
    }
